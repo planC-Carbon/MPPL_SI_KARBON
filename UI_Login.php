26dc9f46e7b0657236a9584b8fddb63553492410
afinalina <afina.lina12@gmail.com>
2015-03-02 09:16:00 +0800

    Create UI_Login.php

---
+++ UI_Login.php
@@ -0,0 +1,33 @@
+<html>
+<body>
+<head>
+	<title>Form Login</title>
+	<link rel="stylesheet" type="text/css" href="Art/format.css">
+	<link rel="stylesheet" type="text/css" href="Art/style.css">
+</head>
+<div id="main">
+	<div id="judul">
+			<div class="mahasiswa"><a href="">Apasih C-Carbon?</a></div>
+	</div>
+	<div id="content">
+		
+		<div id="content-main">
+			<form>
+				<fieldset>
+				<br><div class="text">Username</div><div class="Input"><input type="password" name="NRP" placeholder="username"></div>
+				<br><div class="text">Password</div><div class="Input"><input type="password" name="NAMA" placeholder="password"></div>
+				<br><br>
+				<div class="button-group">
+					<a href="" class="button facebook">Login</a>
+				</div>
+				</fieldset>
+			</form>
+		</div>
+	</div>
+<div id="footer">
+<div id="gambar"><img src="Picture/siklus.jpg" width="50px" height="50px"></div>
+<div id="gambar"><img src="Picture/ITS.png" width="50px" height="50px"></div>
+<p>Copyright &copy;2014 SIKP-ITS.All rights reserved - Bantuan - ITS Online 
+</div>
+</body>
+</html>
