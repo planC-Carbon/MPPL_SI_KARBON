a9be02076484a92fbb58457054f36f3ef96c9b51
afinalina <afina.lina12@gmail.com>
2015-03-06 15:01:36 +0800

    Update and rename application to application/view/login_view.php

---
+++ application/view/login_view.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="../styles.css">
+<title>Login Anggota</title>
+<link rel="icon" type="image/png" href="../images/saveKBS.png">
+</head>
+<body background="../images/siteBackground.jpg">
+
+<div class="container-narrow">
+<div class="well" align="center">
+   <h1>Login Anggota</h1>
+   <?php echo validation_errors(); ?>
+   <?php echo form_open('verifylogin'); ?>
+     <label for="username">Username:</label>
+     <input type="text" size="20" id="username" name="username"/>
+     <br/>
+     <label for="password">Password:</label>
+     <input type="password" size="20" id="passowrd" name="password"/>
+     <br/>
+     <input type="submit" value="Login"/>
+   </form>
+   </div>
+   </div>
+ </body>
+</html>
