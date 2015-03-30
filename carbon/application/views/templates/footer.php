dc1798a4a7bb9dd1d1632f0421bafbe0d8688bb7
frhdina <izdiharfarahdina@gmail.com>
2015-03-30 08:42:40 +0700

    update

---
+++ carbon/application/views/templates/footer.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<!--
+Design by Free Responsive Templates
+http://www.free-responsive-templates.com
+Released for free under a Creative Commons Attribution 3.0 Unported License (CC BY 3.0) 
+-->
+<html>
+<footer>
+	
+</footer>
+</html>
---
+++ carbon/application/views/templates/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<!--
+Design by Free Responsive Templates
+http://www.free-responsive-templates.com
+Released for free under a Creative Commons Attribution 3.0 Unported License (CC BY 3.0) 
+-->
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>C-Carbon - Home</title>
+	<link rel="icon" type="image/jpg" href="../../assets/images/siklus.jpg">
+	<link rel="stylesheet" href="<?= base_url()?>assets/css/styles.css"/>
+	<link rel="stylesheet" href="<?= base_url()?>assets/css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="<?= base_url()?>assets/css/font.css"/>
+	<link rel="stylesheet" href="<?= base_url()?>assets/style3.css"/>
+	<link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Belgrano' rel='stylesheet' type='text/css'>
+	<script src="<?=base_url()?>assets/js/jquery-1.7.1.min.js"></script>
+	<script src="<?=base_url()?>assets/js/jquery.flexslider-min.js"></script>
+</head>
+<body background="../../assets/images/siteBackground.jpg" >
+<header background="../../assets/images/headerTop.png">
+</header>
